9ef2ef3ec99bc4b991406543290a4157e58d0470
lamnq2004 <minh81616@gmail.com>
2024-05-29 17:46:31 +1000

    Update OrderServices.cs
    Update functions

--- Application/Services/OrderServices.cs
+++ Application/Services/OrderServices.cs
@@ -2,6 +2,7 @@
 using Application.Abstractions.Services;
 using Application.Entities.Auth;
 using Application.Entities.Ordering;
+using System.Security.Principal;
 
 namespace Application.Services
 {
@@ -12,6 +13,42 @@ namespace Application.Services
         public OrderServices(IOrderRepository orderRepository)
         {
             _repo = orderRepository;
+        }
+
+        public Order CreateOrder(Order newOrder)
+        {
+            if (newOrder == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            _repo.Add(newOrder);
+            return newOrder;
+        }
+
+        public List<Order> GetAllOrders(Account account)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException(nameof(account));
+            }
+
+            if (account.AccountType != AccountType.Staff)
+            {
+                throw new UnauthorizedAccessException("Only admin accounts can retrieve order information.");
+            }
+
+            return _repo.GetAll();
+        }
+
+        public Order GetOrderById(Guid id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            _repo.Delete(id);
         }
     }
 }
