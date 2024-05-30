06aa8347c71ab76dc29637998858c49d0eb5aed4
pkduongsu <62507699+pkduongsu@users.noreply.github.com>
2024-05-30 17:10:15 +1000

    add login and order

---
+++ webapp/src/components/MenuItem.jsx
@@ -0,0 +1,21 @@
+export default function MenuItem({
+    image, name, price 
+}) {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden">
+            <img src={image} alt={name} className="w-full h-48 object-cover" />
+            <div className="p-4">
+              <h2 className="text-xl font-bold">{name}</h2>
+              <p className="text-gray-700">{price}</p>
+              <button className="mt-4 w-full bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+      </div>
+    );
+}
+   
+
+
--- webapp/src/pages/order/order.jsx
+++ webapp/src/pages/order/order.jsx
@@ -1,7 +1,32 @@
+import MenuItem from '../../components/MenuItem.jsx';
+import React from "react";
+import bibimbap from "../../images/bibimbap.jpg";
+
 const Order = () => {
-    return (
-        <div>Order</div>
-    )
-}
+
+    const menuItems = [
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+        { name: "Bibimbap", price: "$16.8", image: bibimbap },
+      ];
+
+    return (     
+            <div className="bg-white p-8">
+              <div className="flex justify-between items-center mb-8">
+                <h1 className="text-4xl font-bold text-red-600">Dine In Menu</h1>
+                <div className="bg-red-600 text-white py-2 px-4 rounded">
+                  Table 5
+                </div>
+              </div>
+              <p className="text-gray-600 mb-8">
+                Enjoy our various selections from different cuisines.
+              </p>
+              <MenuItem />
+            </div>
+          );
+};
 
 export default Order;
