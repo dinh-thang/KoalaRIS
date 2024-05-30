2bc963a815f6c024d11a9f054aa67f47184b33cd
pkduongsu <62507699+pkduongsu@users.noreply.github.com>
2024-05-30 23:05:34 +1000

    cart components

--- webapp/src/pages/order/order.jsx
+++ webapp/src/pages/order/order.jsx
@@ -1,9 +1,11 @@
 import MenuItem from '../../components/MenuItem.jsx';
-import React from "react";
+import { React, useState } from "react";
 import bibimbap from "../../images/bibimbap.jpg";
+import Cart from "../../components/Cart.jsx";
 
 const Order = () => {
 
+    // Replace this with DB
     const menuItems = [
         { name: "Bibimbap", price: "$16.8", image: bibimbap },
         { name: "Bibimbap", price: "$16.8", image: bibimbap },
@@ -13,24 +15,44 @@ const Order = () => {
         { name: "Bibimbap", price: "$16.8", image: bibimbap },
       ];
 
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (menuItem) => {
+        setCartItems([...cartItems, menuItem]);
+    };
+
+    const removeFromCart = (index) => {
+        const newCartItems = [...cartItems];
+        newCartItems.splice(index, 1);
+        setCartItems(newCartItems);
+    };
+
     return (     
-        <div className="bg-white p-8">
-            <div className="flex justify-between items-center mb-8">
+            <div className="bg-white p-8">
+              <div className="flex justify-between items-center mb-8">
                 <h1 className="text-4xl font-bold text-red-600">Dine In Menu</h1>
                 <div className="bg-red-600 text-white py-2 px-4 rounded">
                   Table 5
                 </div>
-            </div>
-            <p className="text-gray-600 mb-8">
+              </div>
+              <p className="text-gray-600 mb-8">
                 Enjoy our various selections from different cuisines.
-            </p>
+              </p>
 
             {/* Replace Menu Items array with db */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                {menuItems.map(item => (<MenuItem {...item} />) )}
+                {menuItems.map((item) => (<MenuItem {...item} />))}
             </div>
-        </div>
-        );
+            
+            {cartItems.length > 0 && (
+            <Cart 
+                cartItems={cartItems} 
+                removeFromCart={removeFromCart} 
+            />
+      )}
+
+    </div>
+    );
 };
 
 export default Order;
